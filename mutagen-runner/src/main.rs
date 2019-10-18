986e4ae3d371d554c95873bfbfdb4d6ab0594307
Samuel Pilz <samuel.pilz@posteo.net>
2019-10-18 15:56:30 +0200

    treat missing coverage file as empty

--- mutagen-runner/src/main.rs
+++ mutagen-runner/src/main.rs
@@ -163,7 +163,7 @@ fn read_mutations() -> Fallible<Vec<BakedMutation>> {
 fn read_coverage() -> Fallible<HashSet<usize>> {
     let coverage_file = comm::get_coverage_file()?;
     if !coverage_file.exists() {
-        bail!("file `target/mutagen/coverage` is not found")
+        return Ok(HashSet::new()) // no coverage file means that no mutations has been covered
     }
 
     let coverage_hits = comm::read_items::<CoverageHit, _>(coverage_file)?;
