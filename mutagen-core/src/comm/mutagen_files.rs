a9104109c459169eb1a64c0b40d93d4171a6c211
R. Tony Goold <tony@goold.net>
2022-05-16 16:16:54 -0300

    Replace deprecated failure crate with anyhow (#186)
    * Replace deprecated failure crate with anyhow
    
    This commit fixes #185
    
    * fixup! Replace deprecated failure crate with anyhow

--- mutagen-core/src/comm/mutagen_files.rs
+++ mutagen-core/src/comm/mutagen_files.rs
@@ -1,4 +1,4 @@
-use failure::{bail, format_err, Fallible};
+use anyhow::{bail, Context, Result};
 use serde::{de::DeserializeOwned, Serialize};
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
@@ -13,20 +13,20 @@ const JSON_MUTAGEN_FILENAME: &str = "mutations.json";
 const DEFAULT_COVERAGE_FILENAME: &str = "coverage";
 
 /// Finds the file that contains the descriptions of all mutations as written by the procedural macro
-pub fn get_mutations_file() -> Fallible<PathBuf> {
+pub fn get_mutations_file() -> Result<PathBuf> {
     Ok(mutagen_dir()?.join(DEFAULT_MUTAGEN_FILENAME))
 }
 
-pub fn get_mutations_file_json() -> Fallible<PathBuf> {
+pub fn get_mutations_file_json() -> Result<PathBuf> {
     Ok(mutagen_dir()?.join(JSON_MUTAGEN_FILENAME))
 }
 
-pub fn get_coverage_file() -> Fallible<PathBuf> {
+pub fn get_coverage_file() -> Result<PathBuf> {
     Ok(mutagen_dir()?.join(DEFAULT_COVERAGE_FILENAME))
 }
 
 /// queries `cargo` for the workspace root and locates the directory to write mutagen-specific information
-fn mutagen_dir() -> Fallible<PathBuf> {
+fn mutagen_dir() -> Result<PathBuf> {
     let metadata = Command::new("cargo").arg("metadata").output()?;
     if !metadata.status.success() {
         bail!("{}", str::from_utf8(&metadata.stderr)?);
@@ -35,21 +35,19 @@ fn mutagen_dir() -> Fallible<PathBuf> {
     let root_dir = Path::new(
         meta_json["workspace_root"]
             .as_str()
-            .ok_or_else(|| format_err!("cargo metadata misses workspace_root"))?,
+            .with_context(|| "cargo metadata misses workspace_root")?,
     );
     Ok(root_dir.join(DEFAULT_MUTAGEN_DIR))
 }
 
-pub fn read_items<T: DeserializeOwned>(filepath: &Path) -> Fallible<Vec<T>> {
+pub fn read_items<T: DeserializeOwned>(filepath: &Path) -> Result<Vec<T>> {
     BufReader::new(File::open(filepath)?)
         .lines()
-        .map(|line| {
-            serde_json::from_str(&line?).map_err(|e| format_err!("mutation format error: {}", e))
-        })
+        .map(|line| serde_json::from_str(&line?).with_context(|| "mutation format error"))
         .collect()
 }
 
-pub fn append_item<T: Serialize + ?Sized>(file: &mut File, item: &T) -> Fallible<()> {
+pub fn append_item<T: Serialize + ?Sized>(file: &mut File, item: &T) -> Result<()> {
     let mut w = BufWriter::new(file);
     serde_json::to_writer(&mut w, item)?;
     writeln!(&mut w)?; // write newline
